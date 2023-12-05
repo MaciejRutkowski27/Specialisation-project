b193d5635d6ae086ca4bfefb2797f25f4e370e36
varazanina <113165723+varazanina@users.noreply.github.com>
2023-12-05 12:08:53 +0100

    firebase

---
+++ src/config/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-SEoc7D5HzwsnGzPQ9yYs3n46KZCLvmw",
+  authDomain: "trip-simple-20c18.firebaseapp.com",
+  projectId: "trip-simple-20c18",
+  storageBucket: "trip-simple-20c18.appspot.com",
+  messagingSenderId: "505505123657",
+  appId: "1:505505123657:web:f8afd243c3fa1f22c75e98",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
