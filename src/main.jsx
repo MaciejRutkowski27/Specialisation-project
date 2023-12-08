2736fbd5a798b1be98060203e5a433dfefb35fe5
varazanina <113165723+varazanina@users.noreply.github.com>
2023-12-08 13:07:26 +0100

    removing basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename={
-        import.meta.env.DEV ? "/" : "/react-vite-rest-post-app-with-auth/"
-      }
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
