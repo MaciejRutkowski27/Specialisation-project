05bca53740a1691923f1e330f35acfbca8543903
varazanina <113165723+varazanina@users.noreply.github.com>
2023-12-14 10:30:33 +0100

    css of home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,22 @@
 import { Navigation } from "../components/Navigation";
 import CTA_image from "../assets/CTA section.webp";
 import { TopPart } from "../components/TopPart";
+import { Link } from "react-router-dom";
+import "./homePage.css";
 
 export const HomePage = () => {
   return (
     <section>
       <Navigation />
       <TopPart />
-      <section>
-        <img src={CTA_image} alt="Travel with us" />
+      <section style={{ position: "relative" }}>
+        <img className="picture" src={CTA_image} alt="Travel with us" />
+        <div className="overlay-content general-margin">
+          <h1>Plan your next trip!</h1>
+          <Link to="/create">
+            <button>Start planning</button>
+          </Link>
+        </div>
       </section>
     </section>
   );
